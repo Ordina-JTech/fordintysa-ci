86ceab3ce175bc3ed0a6ab1a1f78c8b7cf73e008
Bert Koorengevel <bertkoor@gmail.com>
2017-01-17 23:02:06 +0100

    move current dir to persistent location

--- open.sh
+++ open.sh
@@ -4,24 +4,31 @@ grn='\033[0;32m'
 gry='\033[0;37m'
 yel='\033[1;33m'
 whi='\033[1;37m'
+if [ `pwd` == "/home/docker" ]
+  then
+    mydir="./fordintys-ci/"
+  else
+    mydir="./"
+fi
 if [ `whoami` != 'root' ]
   then
-    echo -e "${yel}Insufficient privileges to cast the ${red}open${yel} spell!"
-    echo -e "${gry}try ${grn}sudo ./open.sh${gry} instead."
+    echo -e "${yel}Insufficient privileges."
+    echo -e "${gry}Unable to cast the ${red}open${gry} spell. Maybe try this:"
+    echo -e "   ${whi}sudo{gry} ./fordintys-ci/open.sh"
     exit
 fi
 if [ $# -lt 1 ]
   then
-    echo -e "${yel}Mandatory argument missing.${gry}"
+    echo -e "${yel}Required parameter missing.${gry}"
     echo "Open what, Ali Baba? Available files:"
-    ls -1
+    ls -1 $mydir
     exit
 fi
-if [ ! -r $1 ]
+if [ ! -r ${mydir}$1 ]
   then
-    echo -e "${yel}File '$1' not found!${gry}"
+    echo -e "${yel}$1: file or directory not found.${gry}"
     echo "Open what, Ali Baba? Available files:"
-    ls -1
+    ls -1 $mydir
     exit
 fi
 echo ""
@@ -32,3 +39,10 @@ echo " [ ]  |  ||   |   | \|     \|      \|  ||  ||     __   [ ]"
 echo " [ ]  \__/|   |___|  |   __/|___ __/|  ||  ||___  \/   [ ]"
 echo "O===O                                                 O===O"
 echo ""
+if [ `pwd` == "/home/docker" ] -a [ -d /mnt/sda1 ] -a [ ! -d /mnt/sda1/fordintys-ci ]
+  then
+    echo "Moving fordintys-ci from a volatile to a persistent location"
+    mv fordintys-ci /mnt/sda1/
+    echo -e "Current directory is changed to ${whi}/mnt/sda1/fordintys-ci${gry}"
+    cd /mnt/sda1/fordintys-ci
+fi
