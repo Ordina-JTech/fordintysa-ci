ef9c42e82987b14f15234085357c5e65db863d3a
Bert Koorengevel <bertkoor@gmail.com>
2017-01-17 23:20:45 +0100

    run docker compose

--- open.sh
+++ open.sh
@@ -56,3 +56,6 @@ if [ -r /usr/local/bin/docker-compose ]
     curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 fi
+# run docker-compose
+docker-compose -f=$1 up -d
+ 
