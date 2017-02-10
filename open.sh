93017c3a4d386e157d469466fd0edc3695faec8e
Bert Koorengevel <bertkoor@gmail.com>
2017-02-10 13:06:41 +0100

    ; then

--- open.sh
+++ open.sh
@@ -2,8 +2,7 @@
 
 ## is docker installed?
 docker --version 1> /dev/null
-if [ $? -ne 0 ]
-  then
+if [ $? -ne 0 ]; then
     echo "Error: docker not installed."
     exit
 fi
@@ -23,24 +22,21 @@ echo -e "${whi}O===O ${cya} __  __  ___     ${yel}  __  ___ __  __      ___ ${cy
 docker-compose version > /dev/null 2>&1
 composeretc=$?
 needroot=$composeretc
-if [ $needroot -gt 0 ] && [ `whoami` != 'root' ]
-  then
+if [ $needroot -gt 0 ] && [ `whoami` != 'root' ]; then
     echo -e "${yel}Insufficient privileges."
     echo -e "${gry}Unable to cast the ${red}open${gry} spell. Maybe try this:"
     echo -e "   ${whi}sudo${gry} ./open.sh"
     exit
 fi
 echo -e "${whi} [ ]  ${cya}/  \|  \|   |  | ${yel} /   |   /   /  \|  ||    ${cya}|  | ${whi} [ ]"
-if [ $# -lt 1 ]
-  then
+if [ $# -lt 1 ]; then
     echo -e "${yel}Required parameter missing.${gry}"
     echo "Open what, Ali Baba? The current directory contains:"
     ls -1 *.*
     exit
 fi
 echo -e "${whi} [ ]  ${cya}|  ||__/|__ |\ | ${yel} \__ |__ \__ |__||\/||__  ${cya} \/  ${whi} [ ]"
-if [ ! -r $1 ]
-  then
+if [ ! -r $1 ]; then
     echo -e "${yel}$1: file or directory not found.${gry}"
     echo "Open what, Ali Baba? The current directory contains:"
     ls -1 *.*
@@ -54,8 +50,7 @@ echo -e "${gry}"
 ## check the ip address
 ip=`ip route | awk '/eth1/ { print $9 }'`
 # empty result?
-if [ ${ip}"x" = "x" ]
-  then
+if [ ${ip}"x" = "x" ]; then
     # not VirtualBox, try Mac approach
 	ip=`ifconfig | grep "inet ." | awk '/broadcast/ { print $2 }'`
 fi
@@ -68,20 +63,17 @@ case "$ip" in
     echo -n "Is this correct? (y/n) : "
     read -n 1 answer
     echo ""
-    if [ $answer != "y" ] && [ $answer != "Y" ]
-      then
+    if [ $answer != "y" ] && [ $answer != "Y" ]; then
         echo -n "Enter your ip address : "
     	read ip
     fi
 esac
 
 docker-compose version 2> /dev/null
-if [ $composeretc -ne 0 ]
-  then
+if [ $composeretc -ne 0 ]; then
     echo -e "${whi}Installing docker-compose${gry}"
     curl -L https://github.com/docker/compose/releases/download/1.10.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-	if [ $? -ne 0 ]
-	  then
+	if [ $? -ne 0 ]; then
 	    echo "Sorry, something went wrong..."
 		exit
 	fi
@@ -90,28 +82,24 @@ fi
 
 # prepare files for docker-compose
 if [ ! -f docker-compose.yml ]; then
-  ln -s sesame.yml docker-compose.yml
+    ln -s sesame.yml docker-compose.yml
 fi
 mkdir ./homepage/tmp 2> /dev/null
 cat ./homepage/default.conf | sed s/$\{ip\}/$ip/ > ./homepage/tmp/default.conf
 
 clone=y
-if [ -d ./gitblit/tmp/OrdinaJTech ]
-  then
+if [ -d ./gitblit/tmp/OrdinaJTech ]; then
     echo -n "Remove and re-clone repositories from GitHub? (y/n) : "
     read -n 1 clone
-    if [ $clone = "y" ] || [ $clone = "Y" ]
-      then
+    if [ $clone = "y" ] || [ $clone = "Y" ]; then
         rm -rf ./gitblit/tmp
     fi
 fi
 echo ""
-if [ $clone = "y" ] || [ $clone = "Y" ]
-  then
+if [ $clone = "y" ] || [ $clone = "Y" ]; then
     echo -e "${whi}Clone some GitHub repositories...${gry}"
     git clone https://github.com/ordina-jtech/fordintysa-ci.git ./gitblit/tmp/OrdinaJTech/fordintysa-ci.git --bare
-    if [ $? -ne 0 ]
-      then
+    if [ $? -ne 0 ]; then
         echo "Sorry, something went wrong..."
         exit
     fi
@@ -131,8 +119,7 @@ fi
 # build the composition
 echo -e "${whi}Start docker-compose build...${gry}"
 docker-compose build
-if [ $? -ne 0 ]
-  then
+if [ $? -ne 0 ]; then
     echo "Sorry, something went wrong..."
     exit
 fi
